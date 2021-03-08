6188068faa117e0324ab44651f230e7e32da9981
Peng Chen <pchen0816@outlook.com>
2021-03-08 10:28:18 -0800

    deleted unnecessary files

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,14 +2,10 @@
 module.exports = (req, res, next) => {
 
   let ip = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress || 'No IP detected';
-  let reqRecord = {
-    ip: ip,
-    method: req.method,
-    time: req.requestTime,
-    targetUrl: req.path,
-  };
+  const time = new Date();
 
-  console.log('\n', `Web request from IP address: "${ip}" `, '\n', `  Using "${req.method}" method`, '\n', `  At ${req.requestTime}`, '\n', `  The requested URL is "${req.path}"`, '\n');
+
+  console.log('\n', `Web request from IP address: "${ip}" `, '\n', `  Using "${req.method}" method`, '\n', `  At ${time}`, '\n', `  The requested URL is "${req.path}"`, '\n');
   
   next();
 };
