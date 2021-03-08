eed7f7c1bf4e0ecfcd80ec36319fc6b2db067bab
Peng Chen <pchen0816@outlook.com>
2021-03-08 10:21:40 -0800

    readme update

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,11 +1,15 @@
 'use strict';
 module.exports = (req, res, next) => {
 
-  const ip = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress || 'No IP detected';
-  const time = new Date();
+  let ip = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress || 'No IP detected';
+  let reqRecord = {
+    ip: ip,
+    method: req.method,
+    time: req.requestTime,
+    targetUrl: req.path,
+  };
 
-
-  console.log('\n', `Web request from IP address: "${ip}" `, '\n', `  Using "${req.method}" method`, '\n', `  At ${time}`, '\n', `  The requested URL is "${req.path}"`, '\n');
+  console.log('\n', `Web request from IP address: "${ip}" `, '\n', `  Using "${req.method}" method`, '\n', `  At ${req.requestTime}`, '\n', `  The requested URL is "${req.path}"`, '\n');
   
   next();
 };
