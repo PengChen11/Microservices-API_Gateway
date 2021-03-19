b51f45a3637bc4abe50240d821eb4e2a7648f8fb
Peng Chen <pchen0816@outlook.com>
2021-03-18 22:16:57 -0700

    a draw back with serverless deployment

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ global.services = {
     urls:[process.env.AUTH_SERVICE_URL],
     currentIndex: 0,
   },
+  blogService: {
+    urls:[process.env.BLOG_SERVICE_URL],
+    currentIndex: 0,
+  },
+  monitorService: {
+    urls:[process.env.MONITOR_SERVICE_URL],
+    currentIndex: 0,
+  },
 };
 
 
